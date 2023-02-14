7d443206542f51a3873f3220296674ff33fc0c41
Sunwoo <bgmashley@gmail.com>
2023-02-14 20:27:57 +0900

    Day2

---
+++ Day2/분수의_덧셈.py
@@ -0,0 +1,7 @@
+import math
+def solution(numer1, denom1, numer2, denom2):
+    numer = denom1*numer2+denom2*numer1
+    denom = denom1*denom2
+    gcd = math.gcd(denom, numer)
+    answer = [numer//gcd, denom//gcd]
+    return answer
